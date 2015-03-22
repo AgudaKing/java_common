11292535013c22408001a80098e75592a36265e3
AgudaKing <jinguichao@163.com>
2015-03-22 23:38:46 +0800

    init project

---
+++ commons/src/main/java/aguda/king/commons/App.java
@@ -0,0 +1,13 @@
+package aguda.king.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
